f635dbc22742957b42bfe09bfef9597cea4ee594
Victor <victor.onic@outlook.fr>
2024-10-10 11:26:54 +0200

    Affichage du labyrinthe

--- game.py
+++ game.py
@@ -1,28 +1,32 @@
 import sys
- 
 import pygame
 from pygame.locals import *
- 
+
+from labyrinth import Maze
+
 pygame.init()
- 
+
 fps = 60
 fpsClock = pygame.time.Clock()
- 
+
 width, height = 1600, 900
 screen = pygame.display.set_mode((width, height))
- 
+
+labyrinthe = Maze((10, 10))
+
 # Game loop.
 while True:
   screen.fill((0, 0, 0))
-  
+
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
       sys.exit()
-  
+
   # Update.
-  
+
   # Draw.
-  
+  labyrinthe.draw(screen)
+
   pygame.display.flip()
   fpsClock.tick(fps)
--- labyrinth.py
+++ labyrinth.py
@@ -1,12 +1,39 @@
-from mazelib import Maze
+from mazelib import Maze as mz
 from mazelib.generate.Prims import Prims
 from mazelib.solve.BacktrackingSolver import BacktrackingSolver
+import pygame
 
-m = Maze()
-m.generator = Prims(10, 10)
-m.generate()
+class Maze:
+    def __init__(self, size: tuple[int,int]):
+        self.width = size[0] * 2 + 1
+        self.height = size[1] * 2 + 1
 
-m.solver = BacktrackingSolver()
-m.generate_entrances()
-# m.solve()
-print(m)
+        m = mz()
+        m.generator = Prims(size[0], size[1])  # fois 2 + 1
+        m.generate()
+
+        m.generate_entrances()
+        # m.solver = BacktrackingSolver()
+        # m.solve()
+
+        self.maze = str(m)
+
+    def draw(self, screen: pygame.surface):
+        # TODO mettre des marges au labyrinthe
+
+        tile_width = screen.get_width() / self.width
+        tile_height = screen.get_height() / self.height
+
+        x, y = 0, 0
+
+        for tile in self.maze:
+            if tile == '#':
+                pygame.draw.rect(screen, (255, 255, 255), (x, y, tile_width, tile_height))
+            elif tile == ' ':
+                pygame.draw.rect(screen, (10, 10, 10), (x, y, tile_width, tile_height))
+            
+            x += tile_width
+
+            if tile == '\n':
+                x = 0
+                y += tile_height
