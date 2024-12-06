f7c6b53c4a43154738723d2c66917560764a967f
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2024-12-06 15:56:48 +0100

    fix apparition souris

--- assets/code/environment.gd
+++ assets/code/environment.gd
@@ -2,22 +2,22 @@ class_name Env extends Node2D
 
 
 @export_range(1, 20)
-var number_student: int
+var students := 5
 
 @onready var desk = %Desk
+@onready var prof : Agent = %Prof
 
 var student_scene := preload("res://assets/scene/student2.tscn")
-@onready var prof := %Prof
-
 var agents : Array[Agent]
 
 func _ready() -> void:
 	desk.position = Vector2i(get_window().size.x/2-150, 100)
+
 	prof.position = Vector2i(get_window().size.x/2 - 25, get_window().size.y/2 - 25)
-	#agents.append(prof)
-	for i in number_student:
-		var new_student := student_scene.instantiate()
-		new_student.position = Vector2i(100, 0)
+	agents.append(prof)
+
+	for i in students:
+		var new_student : Agent = student_scene.instantiate()
 		add_child(new_student)
 		agents.append(new_student)
 
