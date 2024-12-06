3a8b26f542a8f754ecf9b19493cac42ede3d8b37
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2024-12-06 14:46:55 +0100

    test heritage scene agent

--- assets/code/agent.gd
+++ assets/code/agent.gd
@@ -1,8 +1,11 @@
-class_name Agent extends Area2D
+class_name Agent extends Sprite2D
 
 enum States {WORK, CHASE, COMEBACK, LEAVE, COLLECT}
 
 @onready var env = get_parent()
+@onready var agent : CharacterBody2D = $Agent
+@onready var shape : CollisionShape2D = $Shape
+
 var initial_position : Vector2i
 var state : States
 var speed : float 
--- assets/code/environment.gd
+++ assets/code/environment.gd
@@ -6,7 +6,7 @@ var number_student: int
 
 @onready var desk = %Desk
 
-var student_scene := preload("res://assets/scene/student.tscn")
+var student_scene := preload("res://assets/scene/student2.tscn")
 @onready var prof := %Prof
 
 var agents : Array[Agent]
@@ -14,7 +14,7 @@ var agents : Array[Agent]
 func _ready() -> void:
 	desk.position = Vector2i(get_window().size.x/2-150, 100)
 	prof.position = Vector2i(get_window().size.x/2 - 25, get_window().size.y/2 - 25)
-	agents.append(prof)
+	#agents.append(prof)
 	for i in number_student:
 		var new_student := student_scene.instantiate()
 		new_student.position = Vector2i(100, 0)
