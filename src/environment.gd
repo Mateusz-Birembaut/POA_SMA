7d444209773074c3095ff5dbf48c46d550f0d93e
PattateDouce <102151506+PattateDouce@users.noreply.github.com>
2024-12-09 16:04:51 +0100

    le élèves sont séparés de 5 pixels

--- src/environment.gd
+++ src/environment.gd
@@ -15,14 +15,15 @@ var students : Array[Agent]
 
 
 func _ready() -> void:
-	var x := get_window().size.x/2 - (number_of_students * 50)/2 + 25
+	var x := get_window().size.x/2 - (number_of_students * 55)/2 + 25
 	for i in number_of_students:
 		var new_student : Agent = student_scene.instantiate()
 		new_student.position = Vector2i(x, desk.position.y-40-25-5)
 		add_child(new_student)
 		students.append(new_student)
-		x += 50
-		
+		x += 55
+
+
 func get_closest_student_to_candies() -> Agent:
 	var student_to_chase = null
 	var min_distance = 100000
@@ -34,16 +35,17 @@ func get_closest_student_to_candies() -> Agent:
 				student_to_chase = student
 	return student_to_chase
 
+
 func get_number_ready_student() -> int:
 	var nb_ready = 0
 	for student in students:
 		if student.state == Agent.States.READY:
 			nb_ready += 1
-				
+
 	return nb_ready
-	
+
+
 func signal_go() -> void :
 	for student in students:
 		if student.state == Agent.States.READY:
 			student.state = Agent.States.LEAVE
-				
